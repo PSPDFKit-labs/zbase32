0650df882aa28cdfa42a8f8b289eaacba208f5bd
Alexei Sholik <alcosholik@gmail.com>
2017-01-26 17:57:31 +0100

    Fix Elixir 1.4 warnings

--- mix.exs
+++ mix.exs
@@ -10,9 +10,9 @@ defmodule Zbase32.Mixfile do
      elixir: "~> 1.0",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
-     description: description,
-     package: package,
-     deps: deps]
+     description: description(),
+     package: package(),
+     deps: deps()]
   end
 
   def application do
